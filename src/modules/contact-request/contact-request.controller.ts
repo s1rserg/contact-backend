f101cc8bcc6d0ddea8377c1828cb05ca8e7b8dae
s1rserg <sirserg05@gmail.com>
2025-12-15 12:21:57 +0200

    fix(contact-request): fix contact request controller response dto

--- src/modules/contact-request/contact-request.controller.ts
+++ src/modules/contact-request/contact-request.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { ContactRequestService } from './services/contact-request.service';
 import { CreateContactRequestDto } from './dto/create-contact-request.dto';
 import { ContactRequestResponseDto } from './dto/contact-request-response.dto';
+import { TransformPlainToInstance } from 'class-transformer';
 
 @Controller('contact-request')
 export class ContactRequestController {
@@ -9,6 +10,7 @@ export class ContactRequestController {
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
+  @TransformPlainToInstance(ContactRequestResponseDto)
   create(
     @Body() createContactRequestDto: CreateContactRequestDto,
   ): ContactRequestResponseDto {
